c916a46ebd61b257fe5470697b94bb3a11524ad6
Fabian Stiven Valencia Cordoba <fabian.cordoba@correounivalle.edu.co>
2022-08-21 02:47:18 -0500

    images are separated for the baked mesh

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import './style.css'
+import * as dat from 'lil-gui'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
@@ -7,6 +8,13 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 /**
  * Base
  */
+
+// Debug
+const debugObject = {}
+const gui = new dat.GUI({
+    width: 400
+})
+gui.open( gui._closed ); 
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -31,29 +39,61 @@ gltfLoader.setDRACOLoader(dracoLoader)
 /**
  * Textures
  */
-const bakedTexture = textureLoader.load('baked.jpg')
+const bakedTexture = textureLoader.load('textures/baked/baked.jpg')
 bakedTexture.flipY = false
 bakedTexture.encoding = THREE.sRGBEncoding
+
+let vImages = []
+let hImages = []
+
+for (let index = 0; index < 9; index++) {
+    const vTexture = textureLoader.load(`textures/images/v${index}-color.jpg`)
+    vTexture.flipY = false
+    vTexture.encoding = THREE.sRGBEncoding
+
+    const vMaterial = new THREE.MeshBasicMaterial({ map: vTexture})
+    vImages.push(vMaterial)
+}
+
+for (let index = 0; index < 8; index++) {
+    const hTexture = textureLoader.load(`textures/images/h${index}-color.jpg`)
+    hTexture.flipY = false
+    hTexture.encoding = THREE.sRGBEncoding
+
+    const hMaterial = new THREE.MeshBasicMaterial({ map: hTexture})
+    hImages.push(hMaterial)
+}
 
 /**
  * Materials
  */
+
 // Baked material
 const bakedMaterial = new THREE.MeshBasicMaterial({ map: bakedTexture})
+
 
 /**
  * Model
  */
 gltfLoader.load(
-    'gallery.glb',
+    'models/gallery/gallery.glb',
     (gltf) =>
     {
-        gltf.scene.traverse((child) => {
-            if(child instanceof THREE.Mesh) {
-                child.material = bakedMaterial;
-            }
-        })
         scene.add(gltf.scene)
+
+        const bakedMesh = gltf.scene.children.find((child) => child.name === 'baked')
+        bakedMesh.material = bakedMaterial
+        
+        for (let index = 1; index < vImages.length; index++) {
+            const vMesh = gltf.scene.children.find((child) => child.name === `v${index}`)
+            vMesh.material = vImages[index]
+        }
+
+        for (let index = 1; index < hImages.length; index++) {
+            const hMesh = gltf.scene.children.find((child) => child.name === `h${index}`)
+            hMesh.material = hImages[index]
+        }
+        
     }
 )
 
@@ -85,8 +125,12 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(16, 6, 16)
+camera.position.set(0, 3, 9)
 scene.add(camera)
+
+gui.add(camera.position, 'x' ).min(0).max(16).step(1).name("cameraX");
+gui.add(camera.position, 'y' ).min(0).max(16).step(1).name("cameraY");
+gui.add(camera.position, 'z' ).min(0).max(16).step(1).name("cameraZ");
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
